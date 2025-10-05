e00cfdad69ca1008edfc03080762aa0d581cc50f
Aakash Loyar <aakashloyar@gmail.com>
2025-10-05 13:16:03 +0530

    subscribed services

--- AssetApp/Program.cs
+++ AssetApp/Program.cs
@@ -1,10 +1,14 @@
 using AssetApp.Components;
-
+using AssetApp.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddScoped<DatabaseService>();
+builder.Services.AddScoped<EmployeeService>();
+builder.Services.AddScoped<AssetService>();
 
 var app = builder.Build();
 
