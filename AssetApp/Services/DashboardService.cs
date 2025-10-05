661dc073ff3ae94e7586da42a0382fd881571cd5
Aakash Loyar <aakashloyar@gmail.com>
2025-10-05 20:08:03 +0530

    Dashboard services

---
+++ AssetApp/Services/DashboardService.cs
@@ -0,0 +1,50 @@
+using AssetApp.Data;
+using AssetApp.Models;
+
+namespace AssetApp.Services
+{
+    public partial class DashboardService
+    {
+        private readonly DatabaseService _dbService;
+
+        public DashboardService(DatabaseService dbService)
+        {
+            _dbService = dbService;
+        }
+        public async Task<int> GetTotalAssetsAsync()
+        {
+            return await _dbService.QuerySingleAsync<int>(DashboardQueries.GetTotalAssets);
+        }
+
+        public async Task<Dictionary<string, int>> GetAssetsByTypeAsync()
+        {
+            var results = await _dbService.QueryAsync<(string AssetType, int Count)>(DashboardQueries.GetAssetsByType);
+            return results.ToDictionary(x => x.AssetType, x => x.Count);
+        }
+
+        public async Task<int> GetAssignedAssetsAsync()
+        {
+            return await _dbService.QuerySingleAsync<int>(DashboardQueries.GetAssignedAssets);
+        }
+
+        public async Task<int> GetAvailableAssetsAsync()
+        {
+            return await _dbService.QuerySingleAsync<int>(DashboardQueries.GetAvailableAssets);
+        }
+
+        public async Task<int> GetUnderRepairAssetsAsync()
+        {
+            return await _dbService.QuerySingleAsync<int>(DashboardQueries.GetUnderRepairAssets);
+        }
+
+        public async Task<int> GetRetiredAssetsAsync()
+        {
+            return await _dbService.QuerySingleAsync<int>(DashboardQueries.GetRetiredAssets);
+        }
+
+        public async Task<int> GetSpareAssetsAsync()
+        {
+            return await _dbService.QuerySingleAsync<int>(DashboardQueries.GetSpareAssets);
+        }
+    }
+}
